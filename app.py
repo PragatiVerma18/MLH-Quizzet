33bf7d5264dabb3214e79727a49ab74fbf439e5f
user86 <kshitij.kotasthane@gmail.com>
2020-10-06 19:22:32 +0530

    Added pdf/txt support and quiz template

--- app.py
+++ app.py
@@ -1,30 +1,56 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, url_for
 from flask.globals import request
 from werkzeug.utils import secure_filename
+from PyPDF2 import PdfFileReader
+
+# Constants
+UPLOAD_FOLDER = './pdf/'
+
 
 # Init an app object
 app = Flask(__name__)
-
-UPLOAD_PATH = './pdf/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def index():
     """ The landing page for the app """
-    return render_template('index.html')  # render foe template from ./templates
+    return render_template('index.html')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def file_upload():
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    UPLOAD_STATUS = False
     if request.method == 'POST':
         try:
             uploaded_file = request.files['file']
-            uploaded_file.save(
-                UPLOAD_PATH + secure_filename(uploaded_file.filename))
-            uploaded = True
-            return render_template('index.html', uploaded=uploaded)
+            # Make directory to store uploaded files
+            if not os.path.isdir('./pdf'):
+                os.mkdir('./pdf')
+            # Save uploaded file
+            uploaded_file.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)))
+            UPLOAD_STATUS = True
+
+            # Identify file type and other stuff
+            uploaded_content = None
+            file_exten = uploaded_file.filename.rsplit('.', 1)[1].lower()
+            if file_exten == 'pdf':
+                # TODO: Move PDF2Text conversion to another file
+                print('PDF detected')
+                with open(os.path.join(
+                        app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)), 'rb') as pdf_file:
+                    pdf_reader = PdfFileReader(pdf_file)
+                    uploaded_content = pdf_reader.getPage(0).extractText()
+                    print(uploaded_content)
+            else:
+                # Read text file and store contents
+                pass
+
         except Exception as e:
-            return 'failed to upload file'
+            print(e)
+    return render_template('quiz.html', uploaded=UPLOAD_STATUS, pdftext=uploaded_content)
 
 
 if __name__ == "__main__":
